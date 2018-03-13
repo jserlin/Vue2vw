91c51ba712f7cfbf2fa0fbdfccc724f11275c4d9
林峰 <jserlin@163.com>
2018-03-13 17:41:27 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Vue2vw
-Vue项目移动端vw适配
+# Vue项目移动端vm适配方案
+
+> 解决的问题
+- 处理引入的文件和资源路径的处理以及工作模式 postcss-import  postcss-url
+- 压缩和清理代码 cssnano  
+- css特性 兼容性处理 postcss-next
+- 处理元素容器宽高比 postcss-aspect-ratio-mini
+- 处理移动端1px的解决方案 postcss-write-svg
+ 
+##  参考地址
+[Vue项目中使用vw移动适配](https://www.w3cplus.com/mobile/vw-layout-in-vue.html)
+[手淘过年项目使用到的前端技术](https://www.w3cplus.com/css/taobao-2018-year.html)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/example.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="example">
+    <div class="aa">摩西摩西</div>
+    <div aspectratio w-188-246>
+      <div aspectratio-content>auto</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'example',
+  data () {
+    return {
+      msg: 'example'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+[aspectratio] {
+  position: relative;
+}
+[aspectratio]::before {
+  content: '';
+  display: block;
+  width: 1px;
+  margin-left: -1px;
+  height: 0;
+}
+[aspectratio-content] {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
+[w-188-246] {
+  width: 188px;
+  background-color: red;
+}
+[w-188-246] {
+  aspect-ratio: '188:246';
+}
+@svg 1px-border { 
+  height: 2px; 
+  @rect { 
+     fill: var(--color, black);
+     width: 100%;
+     height: 50%; 
+     }
+ } 
+.example { 
+  border: 1px solid transparent; 
+  border-image: svg(1px-border param(--color #00b1ff)) 2 2 stretch;
+}
+
+</style>
